ab1db243a4c1894caa8c343a118bb9d1aa56a95f
Farhad Tavakoli <farhad.tavakoli81@yahoo.com>
2020-11-21 23:51:02 +0100

    Add project files.

---
+++ Array Learing/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Array_Learing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] Numbers = new int[100];
+           
+            int[,] Table = new int[10, 10]; 
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Table[i, j] = (i + 1) * (j + 1); // Initialization of the array
+                   
+                }
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.SetCursorPosition((i * 4) + 10, j * 2 + 4); // Formating the output
+                    Console.Write("{0} ", Table[j, i]);
+                }
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+
+        }
+    }
+}
